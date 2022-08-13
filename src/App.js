c0f928448ac5ef67e9bf4173d0c08d9455f2b64d
aniketparate <aniketvparate@gmail.com>
2022-08-13 18:41:19 +0530

    feat: imported components to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import Note from './components/Note'
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
-function App() {
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.click} type={props.type} >{props.text}</button>
+//   )
+// }
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [searchText, setSearchText] = useState('')
+  const [darkMode, SetDarkMode] = useState(false)
+
+  const addNote = (text) => {
+    // event.preventDefault()
+    // console.log(text)
+    const date = new Date();
+    const noteObj = {
+      id: notes.length,
+      content: text,
+      date: date.toLocaleDateString(),
+      time: date.toLocaleTimeString()
+    }
+    setNotes(notes.concat(noteObj))
+  }
+
+  const deleteNote = (id) => {
+    console.log('hello')
+    // const newNotes = notes.filter(note => note.id !== id)
+    // setNotes(newNotes)
+    setNotes(notes.filter(note => note.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode && 'dark-mode'}`}>
+      {/* if darkMode is true then add dark-mode to the div */}
+      <div className="container">
+        <Header handleToggleDarkMode={SetDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        {/* <form onSubmit={addNote}>
+            <input value={newNote} onChange={handleNewNote} placeholder="Write here ..."></input>
+            <Button text='Add' type="submit" />
+          </form> */}
+        <NotesList notes={notes.filter(note => note.content.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
